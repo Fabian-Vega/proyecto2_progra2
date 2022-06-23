41b234a9436dd171df5bed9a234cfd9f868efd9f
Barbo2610 <alebartay97@gmail.com>
2022-06-22 18:37:43 -0600

    Update list.hpp

--- src/list.hpp
+++ src/list.hpp
@@ -5,31 +5,47 @@
 namespace grph{
 
 class List{
- public:
+ private:
   class Node{
-   private:
-    std::string  data;
-    double       value;
-    Node*        next;
+   public:
+    std::string data;
+    double      value;
+    Node*       next;
 
-    Node(std::string data, double value = 0, Node* next = nullptr)
+   public:
+    Node(const std::string& data, double value = 0, Node* next = nullptr)
     :data(data),
      value(value),
      next(next){
     }
   };
  private:
-  size_t         count;
-  Node*          first;
-  Node*          last;
+  size_t count;
+  Node*  first;
+  Node*  last;
+
+ public: 
   List()
   :count(0),
-   first(nullptr)
-   {
-    
+   first(nullptr),
+   last(nullptr){  
   }
   ~List(){
 
+  }
+
+ public:
+  inline bool isEmpty()const{
+    return this->first == nullptr;
+  }
+
+ public:
+  void append(const std::string& data){
+    if(this->isEmpty()){
+      this->first = this->last = new Node(data); 
+    }else{
+      this->last = this->last->next = new Node(data);
+    }
   }
 };
 
