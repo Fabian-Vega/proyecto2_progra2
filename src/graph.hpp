5d493ff96d13301579da4c6355bd4031591cd960
Barbo2610 <alebartay97@gmail.com>
2022-06-30 17:23:04 -0600

    Doxygen

--- src/graph.hpp
+++ src/graph.hpp
@@ -7,12 +7,23 @@
 #include <vector>
 #include "vertex.hpp"
 
+
 const size_t INITIAL_CAPACITY = 10;
 const size_t INCREASE_FACTOR = 3;
 
 namespace grph {
 
+/**
+ * @brief Template for a default graph
+ * @tparam DataType Specific type of data the vertex are going to use
+ * @tparam WeightType Specific type of data the links of the graphs are going to use 
+ */
 template<typename DataType, typename WeightType>
+
+/**
+ * @brief 
+ * 
+ */
 class Graph {
  private:
   size_t vertexCount;
@@ -23,6 +34,12 @@ class Graph {
 
 
  public:
+  /**
+   * @brief Construct a new Graph object
+   * 
+   * @param capacity size_t type of data
+   * @param directed 
+   */
   explicit Graph(size_t capacity = INITIAL_CAPACITY, bool directed = false)
   :vertexCount(0),
   capacity(capacity),
@@ -32,16 +49,36 @@ class Graph {
   }
 
  public:
+  /**
+   * @brief 
+   * 
+   * @return true 
+   * @return false 
+   */
   inline bool isEmpty() const {
     return this->vertexCount == 0;
   }
-  
+
+  /**
+   * @brief 
+   * 
+   * @param origin 
+   * @param connection 
+   * @return const WeightType& 
+   */
   const WeightType& operator() (
     const Vertex<DataType, WeightType>* origin,
     const Vertex<DataType, WeightType>* connection) const {
       return origin->getLinkWeight(this->whereIsLink(origin, connection)-1);
   }
 
+  /**
+   * @brief 
+   * 
+   * @param origin 
+   * @param connection 
+   * @return WeightType& 
+   */
   WeightType& operator()(
     Vertex<DataType, WeightType>* origin,
     Vertex<DataType, WeightType>* connection) {
@@ -49,6 +86,12 @@ class Graph {
   }
 
  private:
+  /**
+   * @brief 
+   * 
+   * @param vertex 
+   * @return size_t 
+   */
   size_t whereIsVertex(const Vertex<DataType, WeightType>* vertex) const {
     for (size_t position = 0; position < this->vertexCount; ++position) {
         if (this->vecters[position] == vertex) {
@@ -58,6 +101,13 @@ class Graph {
     return 0;
   }
 
+  /**
+   * @brief 
+   * 
+   * @param origin 
+   * @param connection 
+   * @return size_t 
+   */
   size_t whereIsLink(const Vertex<DataType, WeightType>* origin,
   const Vertex<DataType, WeightType>* connection) const {
     for (size_t position = 0;
@@ -70,6 +120,14 @@ class Graph {
   }
 
  public:
+  /**
+   * @brief 
+   * 
+   * @param origin 
+   * @param connection 
+   * @return true 
+   * @return false 
+   */
   bool isAdjacent(
   Vertex<DataType, WeightType>* origin,
   Vertex<DataType, WeightType>* connection) {
@@ -83,7 +141,13 @@ class Graph {
     || this->adjacencyMatrix[destinPosition][originPosition] != 0;
  
   }
-  
+
+  /**
+   * @brief Get the Neighbors object
+   * 
+   * @param origin 
+   * @return Vertex<DataType, WeightType>** 
+   */
   Vertex<DataType, WeightType>** getNeighbors(
     Vertex<DataType, WeightType>* origin) {
       Vertex<DataType, WeightType>** neighbors = 
@@ -96,6 +160,13 @@ class Graph {
       return neighbors;
   }
 
+  /**
+   * @brief 
+   * 
+   * @param vertex 
+   * @return true 
+   * @return false 
+   */
   bool addVertex(Vertex<DataType, WeightType>* vertex) {
     if (this->whereIsVertex(vertex) != 0) {
       return false;
@@ -109,6 +180,13 @@ class Graph {
     return true;
   }
 
+  /**
+   * @brief 
+   * 
+   * @param vertex 
+   * @return true 
+   * @return false 
+   */
   bool removeVertex(Vertex<DataType, WeightType>* vertex) {
     size_t position = this->whereIsVertex(vertex);
     if (position-- == 0) {
@@ -121,6 +199,15 @@ class Graph {
     return true;
   }
 
+  /**
+   * @brief 
+   * 
+   * @param origin 
+   * @param connection 
+   * @param weight 
+   * @return true 
+   * @return false 
+   */
   bool addLink(Vertex<DataType, WeightType>* origin,
   Vertex<DataType, WeightType>* connection,
   WeightType& weight) {
@@ -148,6 +235,14 @@ class Graph {
     return true;
   }
 
+  /**
+   * @brief 
+   * 
+   * @param origin 
+   * @param connection 
+   * @return true 
+   * @return false 
+   */
   bool removeLink(Vertex<DataType, WeightType>* origin,
   const Vertex<DataType, WeightType>* connection) {
     size_t originPosition = this->whereIsVertex(origin);
@@ -175,6 +270,13 @@ class Graph {
     return false;
   }
 
+   /**
+    * @brief Get the Link object
+    * 
+    * @param origin 
+    * @param connection 
+    * @return WeightType& 
+    */
    WeightType& getLink(
     Vertex<DataType, WeightType>* origin,
     Vertex<DataType, WeightType>* connection) {
@@ -192,6 +294,13 @@ class Graph {
     return origin->getLinkWeight(this->whereIsLink(origin, connection)-1);
   }
 
+  /**
+   * @brief Set the Link object
+   * 
+   * @param origin 
+   * @param connection 
+   * @param weight 
+   */
   void setLink(Vertex<DataType, WeightType>* origin,
   Vertex<DataType, WeightType>* connection,
   WeightType& weight) {
@@ -213,6 +322,11 @@ class Graph {
   }
 
  private:
+  /**
+   * @brief 
+   * 
+   * @param newCapacity 
+   */
   void increaseCapacity(size_t newCapacity = 0) {
     if (newCapacity == 0) {
       newCapacity = INCREASE_FACTOR * this->capacity;
@@ -233,6 +347,13 @@ class Graph {
   }
 
  private:
+  /**
+   * @brief 
+   * 
+   * @param newCapacity 
+   * @return true 
+   * @return false 
+   */
   bool couldIncreaseCapacity(size_t newCapacity) const {
     if (this->adjacencyMatrix.size() != newCapacity) {
       return false;
@@ -246,6 +367,11 @@ class Graph {
     return true;
   }
 
+  /**
+   * @brief 
+   * 
+   * @param vertex 
+   */
   void findRemove(Vertex<DataType, WeightType>* vertex) {
     for (size_t current = 0; current < this->vertexCount; ++current) {
       if (this->vecters[current] != vertex) {
