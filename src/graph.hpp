7c8ef7d34c6e24933922ab77bba1cf88eaefeffc
Barbo2610 <alebartay97@gmail.com>
2022-07-01 03:10:44 -0600

    Update graph.hpp

--- src/graph.hpp
+++ src/graph.hpp
@@ -12,6 +12,12 @@
 const size_t INITIAL_CAPACITY = 10;
 const size_t INCREASE_FACTOR = 3;
 
+/*!
+ *  \addtogroup grph
+ *  @{
+ */
+
+//! Generic record interfaces and implementations
 namespace grph {
 
 /**
@@ -53,7 +59,11 @@ class Graph {
   vertexes(capacity, nullptr),
   isDirected(directed) {
   }
-
+  /**
+   * @brief Construct a new Graph object
+   * 
+   * @param other 
+   */
   Graph(const Graph<DataType, WeightType>& other)
   :vertexCount(other.vertexCount),
   capacity(other.capacity),
@@ -61,7 +71,11 @@ class Graph {
   vertexes(other.vertexes),
   isDirected(other.isDirected) {
   }
-
+  /**
+   * @brief Construct a new Graph object
+   * 
+   * @param other 
+   */
   Graph(Graph<DataType, WeightType>&& other)
   :vertexCount(other.vertexCount),
   capacity(other.capacity),
@@ -72,7 +86,10 @@ class Graph {
     other.capacity = 0;
     other.isDirected = false;
   }
-
+  /**
+   * @brief Destroy the Graph object
+   * 
+   */
   ~Graph() {
     this->vertexCount = 0;
     this->capacity = 0;
@@ -86,27 +103,39 @@ class Graph {
   }
 
  public:
+  /**
+   * @brief Operator= overload with constants
+   * @param other A reference to a graph type of object 
+   * 
+   * @return Graph<DataType, WeightType>& 
+   */
   Graph<DataType, WeightType>& operator=(const
   Graph<DataType, WeightType>& other) {
-  if (this != &other) {
-    if (this->capacity != other.capacity) {
-      for (size_t column = 0; column < this->adjacencyMatrix.size();
-      ++column) {
-      this->adjacencyMatrix[column].resize(other.capacity);
+    // Conditions in case this is diferent from the other reference
+    if (this != &other) {
+      //
+      if (this->capacity != other.capacity) {
+        for (size_t column = 0; column < this->adjacencyMatrix.size();
+        ++column) {
+          this->adjacencyMatrix[column].resize(other.capacity);
+        }
+        this->adjacencyMatrix.resize(other.capacity);
+        this->vertexes.resize(other.capacity);
+        this->capacity = other.capacity;
       }
-      this->adjacencyMatrix.resize(other.capacity);
-      this->vertexes.resize(other.capacity);
-      this->capacity = other.capacity;
+      this->adjacencyMatrix = other.adjacencyMatrix;
+      this->vertexes = other.vertexes;
+      this->vertexCount = other.vertexCount;
+      this->isDirected = other.isDirected;
     }
-
-    this->adjacencyMatrix = other.adjacencyMatrix;
-    this->vertexes = other.vertexes;
-    this->vertexCount = other.vertexCount;
-    this->isDirected = other.isDirected;
+    return *this;
   }
-  return *this;
-}
-
+  /**
+   * @brief Operator= overload
+   * @details see @a operator= for the other param
+   * 
+   * @return Graph<DataType, WeightType>& 
+   */
   Graph<DataType, WeightType>& operator=(Graph<DataType, WeightType>&& other) {
     if (this != &other) {
       std::swap(this->vertexCount, other.vertexCount);
@@ -128,16 +157,28 @@ class Graph {
   inline bool isEmpty() const {
     return this->vertexCount == 0;
   }
-
+  /**
+   * @brief Get the const Vertex Count object
+   * 
+   * @return size_t 
+   */
   inline size_t getVertexCount() const {
     return this->vertexCount;
   }
-
+  /**
+   * @brief Get the Vertexes object
+   * 
+   * @return std::vector<Vertex<DataType, WeightType>*>& 
+   */
   inline std::vector<Vertex<DataType, WeightType>*>&
   getVertexes() {
     return this->vertexes;
   }
-
+  /**
+   * @brief Get the Vertexes object
+   * 
+   * @return const std::vector<Vertex<DataType, WeightType>*>& 
+   */
   inline const std::vector<Vertex<DataType, WeightType>*>&
   getVertexes() const {
     return this->vertexes;
@@ -542,5 +583,5 @@ class Graph {
 };
 
 }  // namespace grph
-
+/*! @} End of Doxygen Groups*/
 #endif  // GRAPH_HPP
