2a5a34e22566f837530263c45dadde732f95442c
Barbo2610 <alebartay97@gmail.com>
2022-07-01 03:37:43 -0600

    make doc management

--- src/graph.hpp
+++ src/graph.hpp
@@ -115,7 +115,8 @@ class Graph {
     if (this != &other) {
       // Conditional in case the capacities are different
       if (this->capacity != other.capacity) {
-        // Cycle that goes from 0 until it reaches the size of the adjacency matrix and resizes it
+        // Cycle that goes from 0 until it reaches
+        // the size of the adjacency matrix and resizes it
         for (size_t column = 0; column < this->adjacencyMatrix.size();
         ++column) {
           this->adjacencyMatrix[column].resize(other.capacity);
@@ -126,7 +127,8 @@ class Graph {
         // Assign the new capacty
         this->capacity = other.capacity;
       }
-      // Assigns new adjacency matrix, vertexes, vertex count and the fact that if its directed or not
+      // Assigns new adjacency matrix, vertexes, vertex count
+      // and the fact that if its directed or not
       this->adjacencyMatrix = other.adjacencyMatrix;
       this->vertexes = other.vertexes;
       this->vertexCount = other.vertexCount;
@@ -142,7 +144,8 @@ class Graph {
    */
   Graph<DataType, WeightType>& operator=(Graph<DataType, WeightType>&& other) {
     if (this != &other) {
-      // Swaps the vertex count, capacity, adjacency matrix, vertexes and if it is directed or not from this to other
+      // Swaps the vertex count, capacity, adjacency matrix, vertexes
+      // and if it is directed or not from this to other
       std::swap(this->vertexCount, other.vertexCount);
       std::swap(this->capacity, other.capacity);
       std::swap(this->adjacencyMatrix, other.adjacencyMatrix);
---
+++ test/blackBoxTesting.cpp
@@ -0,0 +1,95 @@
+/// Copyright [2022] <Alejandro B, Fabian V, Kenneth V>
+
+#include <iostream>
+#include <string>
+
+#include "../src/graph.hpp"
+
+int main(void) {
+  try {
+    grph::Graph<std::string, int> grafo(3, false);
+    grph::Vertex<std::string, int>* boston =
+    new grph::Vertex<std::string, int>((std::string("Boston")));
+    grph::Vertex<std::string, int>* york =
+    new grph::Vertex<std::string, int>((std::string("New York")));
+    grph::Vertex<std::string, int>* vegas =
+    new grph::Vertex<std::string, int>((std::string("Las Vegas")));
+    grph::Vertex<std::string, int>* chicago =
+    new grph::Vertex<std::string, int>((std::string("Chicago")));
+    grph::Vertex<std::string, int>* angeles =
+    new grph::Vertex<std::string, int>((std::string("Los Angeles")));
+    grph::Vertex<std::string, int>* francisco =
+    new grph::Vertex<std::string, int>((std::string("San Franciso")));
+    grph::Vertex<std::string, int>* perez =
+    new grph::Vertex<std::string, int>((std::string("Perez Zeledon")));
+
+    grafo.addVertex(boston);
+    grafo.addVertex(york);
+    grafo.addVertex(vegas);
+    grafo.addVertex(chicago);
+    grafo.addVertex(angeles);
+    grafo.addVertex(francisco);
+    grafo.addVertex(perez);
+
+    int weights[7] = {80, 3, 26, 18, 43, 786345, 69420};
+
+    grafo.addLink(boston, york, weights[0]);
+    grafo.addLink(boston, vegas, weights[1]);
+    grafo.addLink(boston, chicago, weights[2]);
+    grafo.addLink(boston, angeles, weights[3]);
+    grafo.addLink(boston, francisco, weights[4]);
+    grafo.addLink(boston, perez, weights[5]);
+    grafo.setLink(perez, boston, weights[6]);
+
+    grafo.removeVertex(vegas);
+
+    grph::Vertex<std::string, int>** bostonNeighbors =
+    grafo.getNeighbors(boston);
+    std::cout << "\nLas ciudades vecinas de Boston son: \n";
+    for (size_t city = 0; city < boston->getLinkCount(); ++city) {
+      std::cout << bostonNeighbors[city]->getData()
+      << " con una distacia de : " << grafo(boston, bostonNeighbors[city])
+      << std::endl;;
+    }
+    delete [] bostonNeighbors;
+
+    grph::Graph<std::string, int> grafo2(grafo);
+
+    std::cout << "\nLa distancia entre Boston y New York es: "
+    << grafo2.getLink(boston, york) << std::endl;
+    /*std::cout << "\nLa distancia entre Boston y Las Vegas es: "
+    << grafo2.getLink(boston, vegas) << std::endl;*/
+    std::cout << "\nLa distancia entre Boston y Chicago es: "
+    << grafo2.getLink(boston, chicago) << std::endl;
+    std::cout << "\nLa distancia entre Boston y Los Angeles es: "
+    << grafo2.getLink(boston, angeles) << std::endl;
+    std::cout << "\nLa distancia entre Boston y Sam Francisco es: "
+    << grafo2.getLink(boston, francisco) << std::endl;
+    std::cout << "\nLa distancia entre Boston y Perez Zeledón es: "
+    << grafo2.getLink(boston, perez) << std::endl;
+    std::cout << "\nLa distancia entre Perez Zeledón y Boston es: "
+    << grafo2.getLink(perez, boston) << std::endl;
+
+    grph::Vertex<std::string, int>** bNeighboors =
+    grafo.getNeighbors(boston);
+    std::cout << "\nLas ciudades vecinas de Boston son: \n";
+    for (size_t city = 0; city < boston->getLinkCount(); ++city) {
+      std::cout << bNeighboors[city]->getData()
+      << " con una distacia de : " << grafo(boston, bNeighboors[city])
+      << std::endl;;
+    }
+    delete [] bNeighboors;
+
+    delete boston;
+    delete york;
+    delete vegas;
+    delete chicago;
+    delete angeles;
+    delete francisco;
+    delete perez;
+  } catch (const std::runtime_error& error) {
+    std::cerr << "main error: " << error.what() << std::endl;
+  }
+
+  return 0;
+}
