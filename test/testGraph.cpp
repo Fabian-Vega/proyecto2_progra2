4b68cfd3bf945b6648122d7250e39608c86a960c
Fabian-Vega <fabianvega15502@gmail.com>
2022-07-08 16:20:20 -0600

    addded catch2 for adjacencyList

--- test/testGraph.cpp
+++ test/testGraph.cpp
@@ -3,7 +3,7 @@
 #include "../src/graph.hpp"
 
 TEST_CASE("tests"){
-  grph::Graph<std::string, int, true> prueba(3, false);
+  grph::Graph<std::string, int, true> prueba(3, true);
     grph::Vertex<std::string> cities[3] = {
       grph::Vertex<std::string>(std::string("Boston")),
       grph::Vertex<std::string>(std::string("New York")),
@@ -47,4 +47,49 @@ TEST_CASE("tests"){
   // ***Lo cambie a 2 porque deberian haber 2
   REQUIRE(prueba.getVertexCount()== 2);
 
+
+  /// ***Lista enlazada
+  grph::Graph<std::string, int, true> prueba2(3, false);
+    grph::Vertex<std::string> citiesCr[3] = {
+      grph::Vertex<std::string>(std::string("Cartago")),
+      grph::Vertex<std::string>(std::string("Puntarenas")),
+      grph::Vertex<std::string>(std::string("Limon"))
+    };
+  // Should return true because we haven't added anything yet
+  REQUIRE(prueba2.isEmpty()==true);
+  // Should return true because we are adding a non added before vertex
+  REQUIRE(prueba2.addVertex(citiesCr[0]) == true);
+  // Should return false because we already added a vertex
+  REQUIRE(prueba2.isEmpty()==false);
+  // Should return true because we are adding a non added before vertex
+  REQUIRE(prueba2.addVertex(citiesCr[1]) == true);
+  // Should return true because we are adding a non added before vertex
+  grph::Vertex<std::string>& cartago = *prueba2.getVertexes()[0];
+  grph::Vertex<std::string>& puntarenas = *prueba2.getVertexes()[1];
+  REQUIRE(prueba2.addVertex(cartago) == false);
+  // Should return 2 because that is the amount of vertexes that are in the graph
+  REQUIRE(prueba2.getVertexCount()== 2);
+  // Should return true because the non existent link was created succesfully
+  int weightsCr[3] = {801, 367, 72345};
+  REQUIRE(prueba2.addLink(cartago, puntarenas, weightsCr[0])==true);
+  // Should return false because the existent link was already created
+  REQUIRE(prueba2.addLink(cartago, puntarenas, weightsCr[0])==false);
+  // Should return 2 because of the correct creation of the previous link
+  // Should return true because we are adding a non added before vertex
+  // ***Agregue el vertice al vertex
+  REQUIRE(prueba2.addVertex(citiesCr[2]) == true);
+  grph::Vertex<std::string>& limon = *prueba2.getVertexes()[2];
+  // Should return false because the link was already created
+  REQUIRE(prueba2.addVertex(limon) == false);
+  // Should return true because the non existent link was created succesfully
+  REQUIRE( prueba2.addLink(cartago, limon, weightsCr[1])==true);
+  // ***Cambio aqui de 1 a 2
+  REQUIRE(cartago.getLinkCount() == 2);
+  // Should return 801 because of the correct creation of the previous link
+  REQUIRE(prueba2.getLink(cartago, puntarenas) == 801);
+  // Should return true if deleting was  succesful
+  REQUIRE(prueba2.removeVertex(puntarenas)==true);
+  // Should return 1 because that is the amount of vertexes that are in the graph
+  // ***Lo cambie a 2 porque deberian haber 2
+  REQUIRE(prueba2.getVertexCount()== 2);
 }
